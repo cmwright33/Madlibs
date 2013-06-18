50d2ffac94abf8f185a4399373f2c8199c573a9c
Christopher Wright <cmwright33@gmail.com>
2013-06-18 13:13:18 -0400

    update madlibs

---
+++ madlibs.rb
@@ -0,0 +1,27 @@
+puts "give me a noun"
+first_noun = gets.chomp
+puts "give me name"
+name = gets.chomp
+puts "give me a verb"
+first_verb = gets.chomp
+puts "give me marine species"
+marine = gets.chomp
+puts "give me a alcoholic beverage"
+alcohol = gets.chomp
+puts "give me an animal"
+animal = gets.chomp
+puts "give me a name of a building"
+building = gets.chomp
+puts "give a fruit"
+fruit = gets.chomp
+puts "give me your favorite number"
+fav_num = gets.chomp
+puts "give a second name"
+sec_name = gets.chomp
+
+puts "One day #{name}, was walking down the #{first_noun}. " +
+"That's when he/she saw a #{animal}. " +
+"he was drinking a #{alcohol}, which was sort of strange. " +
+"then he #{first_verb}, which was sort of funny " +
+"So then #{sec_name} decided to eat #{fav_num} #{fruit} " +
+"and went swimming with #{marine} in the #{building}!"
